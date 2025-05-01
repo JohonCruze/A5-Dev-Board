42e80a42f56d125cac497832e457b6083d6f7f78
Johon Cruze <johoncruze535@gmail.com>
2025-05-01 12:36:22 +0600

    Create activity log div and added current time

--- scripts/main.js
+++ scripts/main.js
@@ -11,15 +11,36 @@ for (const button of buttons) {
         button.style.cursor = 'not-allowed';
 
         // Decrement task numbers
+
         let decrementTaskNumber = parseInt(taskAssignedButton.innerText);
-        taskAssignedButton.innerText = decrementTaskNumber - 1;
+        decrementTaskNumber = decrementTaskNumber - 1;
+
+        if (decrementTaskNumber < 10) {
+            taskAssignedButton.innerText = '0' + decrementTaskNumber;
+        } else {
+            taskAssignedButton.innerText = decrementTaskNumber;
+        }
 
         // Increment task numbers
         let incrementTaskNumber = parseInt(completedTask.innerText);
         completedTask.innerText = incrementTaskNumber + 1;
 
-        // Getheading
+        // Get heading
         const card = button.parentElement.parentElement.parentElement;
         const heading = card.querySelector('h2');
+
+        // Current Time
+        const time = new Date().toLocaleTimeString('en-US', {
+            hour: '2-digit',
+            minute: '2-digit'
+        });
+
+        // Create activity log
+        const createDiv = document.createElement('div');
+        createDiv.className =
+            'p-5 m-5 bg-[#F4F7FF] rounded text-base font-normal';
+        createDiv.innerText = `You have Complete The Task ${heading.innerText} at ${time}`;
+
+        activityLogDiv.appendChild(createDiv);
     });
 }
